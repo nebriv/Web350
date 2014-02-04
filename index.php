a064d445569b39d8aa36ab5c5c5a9cf87267b936
Ben Virgilio <nebriv@gmail.com>
2014-02-04 16:30:25 -0500

    Almost Got It

--- index.php
+++ index.php
@@ -30,8 +30,8 @@
 			<li class="active"><a href="#">Home</a></li>
 			<li><a href="#">Produce/Shop</a></li>
 			<li><a href="#">Contact Us</a></li>
-			<li><a href="register.php">Sign Up</a></li>
 			<li class="divider-vertical"></li>
+			<li><a href="register.php">Sign Up</a></li>
 			<li class="dropdown">
 				<a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
 				<div class="dropdown-menu" style="padding: 15px; padding-bottom: 10px;">
