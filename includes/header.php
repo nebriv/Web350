cbd0b147a3a767af625e6072b35298013e9ce667
Ben Virgilio <nebriv@gmail.com>
2014-02-24 17:19:34 -0500

    Reorg header dropdown.

--- includes/header.php
+++ includes/header.php
@@ -54,13 +54,14 @@ if ($user->checkCredentials("nebriv2", "pass")){
 					 echo ucfirst($user->getFirstName());
 			echo "<strong class='caret'></strong></a>
 					<div class='dropdown-menu' style='padding: 15px; padding-bottom: 10px;'>
-						<form name='login' action='login.php' method='post' accept-charset='UTF-8'>
-						<input id='user_username' style='margin-bottom: 15px;' type='text' name='username' placeholder='Username' size='30' />
-						<input id='user_password' style='margin-bottom: 15px;' type='password' name='password' placeholder='Password' size='30' />
-						<input id='user_remember_me' style='float: left; margin-right: 10px;' type='checkbox' name='rememberme' value='1' />
-						<label class='string optional' for='user_remember_me'> Remember me</label>
-						<input class='btn btn-primary' style='clear: left; width: 100%; height: 32px; font-size: 13px;' type='submit' name='commit' value='Sign In' />
-						</form>
+						<div class='panel panel-default'>
+							<div class='panel-heading'>
+								<h3 class='panel-title'>Your Account</h3>
+							</div>
+							<div class='panel-body'>
+								Account Info
+							</div>
+						</div>
 					</div>
 				</li>";
 			}else{
