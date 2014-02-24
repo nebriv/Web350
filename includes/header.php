6ad9114e2129adea2d396220ab17d051b98b1e57
Ben Virgilio <nebriv@gmail.com>
2014-02-24 18:54:53 -0500

    Adding more to the menu

--- includes/header.php
+++ includes/header.php
@@ -53,8 +53,9 @@ if ($user->checkSession()){
 					<ul class='dropdown-menu' role='menu' aria-labelledby='dropdownMenu1'>
 							 <li role='presentation' class='dropdown-header'>Your Account</li>
 							<li role='presentation' class='divider'></li>
-							<li role='presentation'><a role='menuitem' tabindex='-1' href='#'>My Orders</a></li>
-							<li role='presentation'><a role='menuitem' tabindex='-1' href='#'>My Settings</a></li>
+							<li role='presentation'><a role='menuitem' tabindex='-1' href='#'>Your Current Subscriptions</a></li>
+							<li role='presentation'><a role='menuitem' tabindex='-1' href='#'>Your Order History</a></li>
+							<li role='presentation'><a role='menuitem' tabindex='-1' href='#'>Your Account Settings</a></li>
 							<li role='presentation' class='divider'></li>
 							<li role='presentation'><a role='menuitem' tabindex='-1' href='logout.php'>Logout</a></li>
 					</ul>
