994890f189ec028b82ba4c8ef9d02231b11e0dd7
Ben Virgilio <nebriv@gmail.com>
2014-02-24 18:28:13 -0500

    Formatting

--- login.php
+++ login.php
@@ -14,7 +14,7 @@ if (isset($_POST['username']) && isset($_POST['password'])){
     
     echo "      <form action='login.php' method='post' accept-charset='UTF-8' class='form-signin' role='form'>
           <h2 class='form-signin-heading'>Please sign in</h2>
-          <br /><div class='alert alert-danger'>Invalid Username or Password</div>
+          <div class='alert alert-danger'>Invalid Username or Password</div>
           <input name='username' type='email' class='form-control' placeholder='Email address' required autofocus>
           <input name='password' type='password' class='form-control' placeholder='Password' required>
           <label class='checkbox'>
