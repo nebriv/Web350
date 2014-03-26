cc5b153b572efd2440f7ef4047dc29e672730276
Ben Virgilio <nebriv@gmail.com>
2014-03-26 15:02:07 -0400

    Basic Settings

--- admin/settings.php
+++ admin/settings.php
@@ -6,7 +6,39 @@ include('includes/header.php');
           <h1 class="page-header">Site Settings</h1>
 
           <h3 class="sub-header">Basic Settings</h3>
-
+          <div class="row">
+            <div class="col-md-1"></div>
+              <div class="well col-md-4">
+                <form class="form-horizontal" role="form">
+                  <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                    <div class="col-sm-10">
+                      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                    <div class="col-sm-10">
+                      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"> Remember me
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <button type="submit" class="btn btn-default">Sign in</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
 
 
           <h3 class="sub-header">Administration Settings</h3>
