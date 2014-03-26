01a2a0709fcf292fb6b82c444c370a648cad053d
Ben Virgilio <nebriv@gmail.com>
2014-03-25 20:38:20 -0400

    Remove Debugging

--- classes/site.class.php
+++ classes/site.class.php
@@ -6,16 +6,14 @@ class Site {
 		$db = buildDBObject();
 		$info = $db->get('Site_Settings');
 		$info = $info[0];
-		#print_r($info);
-		echo $info["SiteName"];
-		#$sitename = $info["SiteName"];
+		$sitename = $info["SiteName"];
 		$MaintenanceMode = $info["MaintenanceMode"];
 		$RegistrationOpen = $info["RegistrationOpen"];
 		$RegistrationOpen = $info["RegistrationOpen"];
 	}
 
 	function siteName(){
-		return $this->$sitename;
+		return $this->sitename;
 	}
 
 
