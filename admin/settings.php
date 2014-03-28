8b3be001fae3427983ad4d48ba142d81d644e808
Ben Virgilio <nebriv@gmail.com>
2014-03-28 15:54:08 -0400

    Checked Logic

--- admin/settings.php
+++ admin/settings.php
@@ -125,10 +125,10 @@ include('includes/header.php');
                   <label>
                     <?php
                       if ($site->maintenanceEnabled()){
-                         echo '<input type="checkbox" id="maintenanceMode" name="maintenanceMode" value="0" checked>
+                         echo '<input type="checkbox" id="maintenanceMode" name="maintenanceMode" value="1" checked>
                         Maintenance Mode Enabled';
                       }else{
-                          echo '<input type="checkbox" id="maintenanceMode" name="maintenanceMode" value="1">
+                          echo '<input type="checkbox" id="maintenanceMode" name="maintenanceMode" value="0">
                         Maintenance Mode Enabled';
                       }
                       ?>
@@ -150,10 +150,10 @@ include('includes/header.php');
                   <label>
                     <?php
                       if ($site->registrationRequired()){
-                         echo '<input type="checkbox" id="registrationRequired" name="registrationRequired" value="0" checked>
+                         echo '<input type="checkbox" id="registrationRequired" name="registrationRequired" value="1" checked>
                         Registration Required';
                       }else{
-                          echo '<input type="checkbox" id="registrationRequired" name="registrationRequired" value="1">
+                          echo '<input type="checkbox" id="registrationRequired" name="registrationRequired" value="0">
                         Registration Required';
                       }
                     ?>
@@ -164,10 +164,10 @@ include('includes/header.php');
                   <label>
                     <?php
                       if ($site->registrationOpen()){
-                         echo '<input type="checkbox" id="registrationOpen" name="registrationOpen" value="0" checked>
+                         echo '<input type="checkbox" id="registrationOpen" name="registrationOpen" value="1" checked>
                         Registration Open';
                       }else{
-                          echo '<input type="checkbox" id="registrationOpen" name="registrationOpen" value="1">
+                          echo '<input type="checkbox" id="registrationOpen" name="registrationOpen" value="0">
                         Registration Open';
                       }
                     ?>
@@ -178,10 +178,10 @@ include('includes/header.php');
                   <label>
                     <?php
                       if ($site->AdminApprovalRequired()){
-                         echo '<input type="checkbox" id="AdminApprovalRequired" name="AdminApprovalRequired" value="0" checked>
+                         echo '<input type="checkbox" id="AdminApprovalRequired" name="AdminApprovalRequired" value="1" checked>
                         Require Administrator Approval of New User Accounts';
                       }else{
-                          echo '<input type="checkbox" id="AdminApprovalRequired" name="AdminApprovalRequired" value="1">
+                          echo '<input type="checkbox" id="AdminApprovalRequired" name="AdminApprovalRequired" value="0">
                         Require Administrator Approval of New User Accounts';
                       }
                     ?>
