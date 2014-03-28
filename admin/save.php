528c4a34fbc62ef72fd3bd618504471ce3e75fa5
Ben Virgilio <nebriv@gmail.com>
2014-03-28 13:59:36 -0400

    Wrong Table

--- admin/save.php
+++ admin/save.php
@@ -17,7 +17,7 @@ if (isset($_GET['what'])){
     }
     if (!empty($data)){
       $db->where('ID', 1);
-      if($db->update('users', $data)) echo 'successfully updated'; 
+      if($db->update('Site_Settings', $data)) echo 'successfully updated'; 
     }
   }
 }
