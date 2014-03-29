db3b31199c4a292dfc61169592c3a61b0b41ba77
Ben Virgilio <nebriv@gmail.com>
2014-03-29 15:16:19 -0400

    Debug

--- logout.php
+++ logout.php
@@ -5,12 +5,13 @@ $user = new User();
 //$user->registerUser($id = NULL, $newusername = NULL, $newemail = NULL, $newfirstname = NULL, $newlastname = NULL, $newrole = "Guest"));
 if ($user->checkSession()){
 	$type = $user->sessionType();
+	echo $type;
 	$user->buildObject($user->checkSession());
 	$log->auth("logoutsuccess", $user);
 	$user->destroySession($type);
-	#echo "You've been logged out.";
+	echo "You've been logged out.";
 
-	header( 'Location: http://csa.nebriv.com' ) ;
+	#header( 'Location: http://csa.nebriv.com' ) ;
 }else{
   $log->auth("logoutfailure", $user);
   echo "You are not logged in";
