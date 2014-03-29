3e5a6187682f73652224cda56ef7763f472dc47d
Ben Virgilio <nebriv@gmail.com>
2014-03-29 15:39:08 -0400

    Debug

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -7,10 +7,12 @@ $site = new Site();
 if ($user->checkSession()){
 	$user->buildObject($user->checkSession());
 	if (!$user->checkPerms(2, false)){
-		header( 'Location: http://csa.nebriv.com' );
+		echo $user->checkPerms(2, false);
+		#header( 'Location: http://csa.nebriv.com' );
 	}
 }else{
-	header( 'Location: http://csa.nebriv.com' );
+	echo "last else";
+	#header( 'Location: http://csa.nebriv.com' );
 }
 ?>
 
