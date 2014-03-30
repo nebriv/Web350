20ae4acecabe08f7e98215897cfbaa7098c2b654
Ben Virgilio <nebriv@gmail.com>
2014-03-30 19:46:34 -0400

    debug

--- admin/edituser.php
+++ admin/edituser.php
@@ -6,6 +6,7 @@ include('includes/header.php');
 $db = buildDBObject();
 $edituser = new User();
 $edituser->buildObject($_GET['username']);
+echo $edituser->getUsername();
 ?>
 
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
