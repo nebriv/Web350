1836d547d1373ce7f4934a8df232fe1fc2a0070f
Ben Virgilio <nebriv@gmail.com>
2014-03-30 19:30:23 -0400

    Formatting

--- admin/edituser.php
+++ admin/edituser.php
@@ -4,46 +4,70 @@ include('includes/header.php');
 
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 class="page-header">Users and Permissions</h1>
-<ol class="breadcrumb">
-  <li><a href="index.php">Dashboard</a></li>
-  <li><a href="perms.php">Users and Permissions</a></li>
-  <li class="active">Editting User</li>
-</ol>
-          <div class="row">
-            <table class="table table-striped table-hover">
-              <tr>
-                <th><input type="checkbox" value=""></th>
-                <th>Username</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Role</th>
-                <th>Edit</th>
-              </tr>
-              <?php
-                $db = buildDBObject();
-                $userIDs = [];
-                $users = $db->rawQuery('SELECT UserID from Users');
-                foreach ($users as $user){
-                  if ($user['UserID'] != 0){
-                    $row = new User();
-                    $row->buildObject($user['UserID']);
-
-                    echo "
-              <tr>
-                <td><input type='checkbox' value=''></td>
-                <td>";echo $row->getUsername(); echo "</td>
-                <td>";echo $row->getFullName(); echo "</td>
-                <td>";echo $row->getEmail(); echo "</td>
-                <td>";echo $row->getRolesAsStrings(); echo "</td>
-                <td><a href='edituser.php?username=".$user['UserID']."'>Edit</a></td>
-              </tr>
-                    ";
-                  }
-                  
-                }
-
-              ?>
-            </table>
+          <ol class="breadcrumb">
+            <li><a href="index.php">Dashboard</a></li>
+            <li><a href="perms.php">Users and Permissions</a></li>
+            <li class="active">Editting User</li>
+          </ol>
+          <div class="col-md-6 pull-left">
+            <form class="form-horizontal" role="form">
+              <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox"> Remember me
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <button type="submit" class="btn btn-default">Sign in</button>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="col-md-6 pull-right">
+            <form class="form-horizontal" role="form">
+              <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox"> Remember me
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <button type="submit" class="btn btn-default">Sign in</button>
+                </div>
+              </div>
+            </form>
           </div>
 
 
