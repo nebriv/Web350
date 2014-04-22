357a70ee4440ed57a6648248500bcd800239d8e9
Ben Virgilio <nebriv@gmail.com>
2014-04-22 19:36:31 -0400

    Undebug

--- admin/products.php
+++ admin/products.php
@@ -25,7 +25,6 @@ include('includes/header.php');
                 $ProductIDs = $db->rawQuery('SELECT ProductID from Products');
                 foreach ($ProductIDs as $product){
                     $row = new Product();
-                    print_r($product);
                     $id = $product["ProductID"];
                     $row->buildObject($id);
 
