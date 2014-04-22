4e77b012e8a22298ca86573f475d9f4a4761448c
dylansaboo <dylansaboo@gmail.com>
2014-04-22 18:56:38 -0400

    Update cartAdd.php

--- cartAdd.php
+++ cartAdd.php
@@ -5,13 +5,13 @@
   if(isset($_SESSION['Farms'])){
     array_push($_SESSION['Farms'], $farm_id);
   }else{
-    $_SESSION['Farms'] = new Array();
+    $_SESSION['Farms'] = array();
     array_push($_SESSION['Farms'], $farm_id);
   }
   if(isset($_SESSION['total'])){
     array_push($_SESSION['total'], $total);
   }else{
-    $_SESSION['total'] = new Array();
+    $_SESSION['total'] = array();
     array_push($_SESSION['total'], $total);
   }
   header(Location: "cart.php");
