2be589a36cdca781ecf1943272009464c88e5b56
Ben Virgilio <nebriv@gmail.com>
2014-04-22 19:09:27 -0400

    Added Comment.

--- cartAdd.php
+++ cartAdd.php
@@ -2,6 +2,8 @@
   $farm_id = $_POST['FarmID'];
   $total = $_POST['total'];
   session_start();
+    //What happens when the user logs in from a different computer? They will loose their saved cart because it is session based.
+  //This should be stored in the database, and tied to their session token located in the Sessions table.
   if(isset($_SESSION['Farms'])){
     array_push($_SESSION['Farms'], $farm_id);
   }else{
