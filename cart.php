145e6433991e9d8a59af92f2369eb5351d7d4ae1
dylansaboo <dylansaboo@gmail.com>
2014-04-22 18:54:33 -0400

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,2 +1,34 @@
 <?php
+include('includes/header.php');
+$db = buildDBObject();
+?>
+
+
+<div class=\"jumbotron\">
+
+<?php
+$CartFarms = $_SESSION['Farms'];
+$totals = $_SESSION['totals'];
+$subtotal = 0;
+foreach($totals as $total){
+  $subtotal+=$total;
+}
+foreach($CartFarms as $CartFarm){
+  $db->get('FarmID', $CartFarm)
+  $farms = $db->get('Farms');
+  foreach($farms as $farm){
+  	echo "<div class=\"caption\">
+          <h3>".$farm['Name']."</h3>
+          <p>";
+          $db->where('FarmID', $farm['FarmID']);
+          $products = $db->get('Products');
+          foreach($products as $product){
+          	echo $product['Name']." $".$product['Price']."<br />";
+          }
+          echo "Total: $".$subtotal;
+          echo "</div>";
+  }
+}
+include('includes/footer.php');
+
 ?>
