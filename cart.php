b52b070e7205f205abee7a990dedfd58ad3f4b58
dylansaboo <dylansaboo@gmail.com>
2014-04-22 19:32:08 -0400

    Update cart.php

--- cart.php
+++ cart.php
@@ -35,8 +35,8 @@ echo "<br /><a href=\"cartClear.php\">Clear Cart</a>
 <input type=\"hidden\" name=\"cmd\" value=\"_xclick\">
 <input type=\"hidden\" name=\"business\" value=\"me@mybusiness.com\">
 <input type=\"hidden\" name=\"currency_code\" value=\"USD\">
-<input type=\"hidden\" name=\"item_name\" value=\"Teddy Bear\">
-<input type=\"hidden\" name=\"amount\" value=\"12.99\">
+<input type=\"hidden\" name=\"item_name\" value=\"Shopping Cart\">
+<input type=\"hidden\" name=\"amount\" value=\"".$subtotal."\">
 <input type=\"image\" src=\"http://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif\" border=\"0\" name=\"submit\" alt=\"Make payments with PayPal - it's fast, free and secure!\">
 </form>";
 include('includes/footer.php');
