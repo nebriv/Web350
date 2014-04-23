821f232563ea87b4a82e02988fdf0bdb341d1726
Ben Virgilio <nebriv@gmail.com>
2014-04-22 20:02:40 -0400

    Change Titles

--- admin/products.php
+++ admin/products.php
@@ -3,10 +3,10 @@ include('includes/header.php');
 ?>
 
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 class="page-header">Users and Permissions</h1>
+          <h1 class="page-header">Product Overview</h1>
 <ol class="breadcrumb">
   <li><a href="index.php">Dashboard</a></li>
-  <li class="active">Users and Permissions</li>
+  <li class="active">Product Overview</li>
 </ol>
           <div class="row">
             <table class="table table-striped table-hover">
