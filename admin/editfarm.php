e12c5a3bd86b9a24fa95ca513daa129207838352
Ben Virgilio <nebriv@gmail.com>
2014-04-22 22:51:20 -0400

    Add edit farms page

---
+++ admin/editfarm.php
@@ -0,0 +1,148 @@
+<?php
+include('includes/header.php');
+
+
+
+$db = buildDBObject();
+$editfarm = new farm();
+$editfarm->buildObject($_GET['farm']);
+
+
+?>
+
+<script>
+  function savefarm(){
+
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var password2 = document.getElementById('password2').value;
+    var fname = document.getElementById('fname').value;
+    var lname = document.getElementById('lname').value;
+
+    var xhr;
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+        xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { // IE 8 and older
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    var data = "what=" + "farm" +  "&username=" + username + "&email=" + email + "&password=" + password + "&password2=" + password2 + "&fname=" + fname + "&lname=" + lname;
+    xhr.open("POST", "http://csa.nebriv.com/admin/save.php", true); 
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");                  
+    xhr.send(data);
+
+    xhr.onreadystatechange = display_data;
+    function display_data() {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          document.getElementById("userMessages").innerHTML = xhr.responseText;
+        } else {
+          document.getElementById("userMessages").innerHTML = xhr.responseText;
+        }
+      }
+    }
+  }
+</script>
+
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">farm Management</h1>
+          <ol class="breadcrumb">
+            <li><a href="index.php">Dashboard</a></li>
+            <li><a href="farms.php">farm Management</a></li>
+            <li class="active">Editting farm</li>
+          </ol>
+          <div class="col-md-4 pull-left">
+          <h3 class="sub-header">farm Details</h3>
+          <div id='userMessages'></div>
+            <form class="form-horizontal" role="form">
+              <div class="form-group">
+                <label for="username" class="col-sm-4 control-label">farm Name</label>
+                <div class="col-sm-8">
+                  <input type="username" class="form-control" id="username" value='<?php echo $editfarm->getName(); ?>'>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="email" class="col-sm-4 control-label">Description</label>
+                <div class="col-sm-8">
+                  <input type="email" class="form-control" id="email" value='<?php echo $editfarm->getDescription(); ?>'>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="password2" class="col-sm-4 control-label">Price</label>
+                <div class="col-sm-8">
+                  <input class="form-control" id="password2" value='$<?php echo $editfarm->getPrice(); ?>'>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="password2" class="col-sm-4 control-label">Quantity</label>
+                <div class="col-sm-8">
+                  <input class="form-control" id="password2" value='<?php echo $editfarm->getQuantity(); ?>'>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="role" class="col-sm-4 control-label">Farm</label>
+                <div class="col-sm-8">
+                <?php
+                  $farms = $editfarm->getFarmsArray();
+                  echo '
+                    <select id="userrole" class="form-control">';
+                    echo "<option value='".$editfarm->getFarmName()."' selected='selected'>".$editfarm->getFarmName()."</option>";
+                    $current = trim($editfarm->getFarmName());
+                    
+                    foreach($farms as $farm){
+                      if ($current != trim($farm["Name"])){
+                        echo "<option value='".$farm["Name"]."'>".$farm["Name"]."</option>";
+                      }
+                    }
+                  echo "
+                    </select>
+                  ";
+
+
+                  ?>
+                </div>
+              </div>
+              <br>
+              <button type="button" class="btn btn-primary btn-sm" onclick="saveUser()">Save Changes</button>
+            </form>
+          </div>
+
+
+          <div class="col-md-7 pull-left">
+<!--             <h3 class="sub-header">User Sessions</h3>
+            <form class="form-horizontal" role="form">
+              <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox"> Remember me
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <button type="submit" class="btn btn-default">Sign in</button>
+                </div>
+              </div>
+            </form> -->
+          </div>
+
+
+        </div>
+
+<?php
+include('includes/footer.php');
+?>
